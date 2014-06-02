9cadb71d5194a9fa1e279d778be83ed85c586e94
emilymye <emily3ye@gmail.com>
2014-06-02 06:15:08 -0700

    attempting fix for xframe security errors

--- config/application.rb
+++ config/application.rb
@@ -8,7 +8,7 @@ Bundler.require(:default, Rails.env)
 
 module Pascal3d
   class Application < Rails::Application
-    config.i18n.enforce_available_locales = false
+    config.action_dispatch.default_headers.clear
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
