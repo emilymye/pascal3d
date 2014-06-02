0efc061dbb505149f160fb450f3bcd5e77ccffef
emilymye <emily3ye@gmail.com>
2014-06-02 04:04:27 -0700

    test for deployed routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Pascal3d::Application.routes.draw do
 
-  get 'mturk/new_annotation/' => "mturk#bounding_box"
+  get 'mturk/test' => "mturk#test"
+
+  get 'mturk/new_annotation' => "mturk#bounding_box"
   get 'mturk/edit_annotation/:annotation_id' => "mturk#edit_annotation"
 
   post 'mturk/test_form' => "mturk#test_form"
