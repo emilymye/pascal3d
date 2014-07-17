1ff1fa388031b665b74775d3673261d3b8fd9ce4
Wonhui Kim <wonhui@stanford.edu>
2014-07-17 00:33:06 -0700

    config/routes.rb url link for mesh task added

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Pascal3d::Application.routes.draw do
   get 'mturk/new_annotation' => "mturk#bounding_box"
+  get 'mturk/mesh' => "mturk#mesh"
+  #get 'mturk/edit_annotation/' => "mturk#mesh"
   get 'mturk/edit_annotation/:annotation_id' => "mturk#edit_annotation"
   post 'mturk/test_form' => "mturk#test_form"
   post 'mturk/default_form' => "mturk#default_form"
