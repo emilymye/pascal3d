6398ab1ee0256fe77b1c51ea05e78db2e7b2a92b
Wonhui Kim <wonhui@stanford.edu>
2014-07-24 15:14:01 -0700

    urls links for orientation/keypoints tasks added

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Pascal3d::Application.routes.draw do
   get 'mturk/new_annotation' => "mturk#bounding_box"
-  get 'mturk/mesh' => "mturk#mesh"
-  #get 'mturk/edit_annotation/' => "mturk#mesh"
   get 'mturk/edit_annotation/:annotation_id' => "mturk#edit_annotation"
+  get 'mturk/mesh_annotation/:annotation_id' => "mturk#mesh_annotation"
+  get 'mturk/orientation_annotation/:annotation_id' => "mturk#orientation_annotation"
+  get 'mturk/keypoints_annotation/:annotation_id' => "mturk#keypoints_annotation"
   post 'mturk/test_form' => "mturk#test_form"
   post 'mturk/default_form' => "mturk#default_form"
 
